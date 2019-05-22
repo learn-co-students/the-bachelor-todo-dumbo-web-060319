bf9f7a0e93bc6a4595fae5a509aebc6333073191
Alexander Schelchere <ascellie@gmail.com>
2019-05-22 17:13:36 -0400

    get contestant name - complete

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,6 +10,14 @@ def find_season(data, season)
     end
 
 end
+# Helper to get to contestants
+def all_contestants(data)
+    # go through data
+    data.find do |season, contestants|
+      return contestants
+    end
+end
+# End of helpers
 
 def get_first_name_of_season_winner(data, season)
   # first name
@@ -28,11 +36,39 @@ first_name
 end
 
 def get_contestant_name(data, occupation)
+  # full name
+  name = ""
+  # go through data
+  data.find do |season, contestants|
+    # go through contestants
+    contestants.find do |contestant|
+      # if the a contestant's occupation is a match, set their name
+      if contestant["occupation"] == occupation
+        name = contestant["name"]
+        return name
+      end
 
+    end
+
+  end
+  # then return it
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  # set count
+  count = 0
+  # get contestants
+  all_contestants(data).find do |contestant|
+    # when a contestant's hometown is a match
+    if contestant["hometown"] == hometown
+      # increment count
+      count += 1
+    end
+
+  end
+  # Return the final count
+  count
 end
 
 def get_occupation(data, hometown)
