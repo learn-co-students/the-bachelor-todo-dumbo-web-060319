9045374eceeba6079d577c968e0e133c6bc77489
Alexander Schelchere <ascellie@gmail.com>
2019-05-22 17:07:28 -0400

    added helper function to find a season

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,33 +1,34 @@
 require 'pry'
 
+# Helper function for finding a season
+def find_season(data, season)
+    # go through our data
+    data.find do |s, info|
+      # If we've gotten to our correct season, 
+      # return the contestants from that season
+      return info if s == season
+    end
+
+end
+
 def get_first_name_of_season_winner(data, season)
   # first name
   first_name = ""
 
-  # go through our data
-  data.find do |s, info|
-    # If we've gotten to our correct season
-    if s == season
-      # Go through the contestants (an array of objects)
-      info.select do |c|
-        # if their status is "Winner"
+  find_season(data, season).select do |c|
+      #  if their status is "Winner"
         if c["status"] == "Winner"
           # extract their first name from "name"
           name_array = c["name"].split(' ')
           first_name = name_array[0]
         end
-
-      end
-
-    end
-
   end
 # return first name
 first_name
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+
 end
 
 def count_contestants_by_hometown(data, hometown)
