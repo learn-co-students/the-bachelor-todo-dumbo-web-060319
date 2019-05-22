359a70524abf3f9fa2e53a5407f5bf07b0e12e3f
Alexander Schelchere <ascellie@gmail.com>
2019-05-22 16:49:33 -0400

    get first name of season winner - complete

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,29 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  # first name
+  first_name = ""
+
+  # go through our data
+  data.find do |s, info|
+    # If we've gotten to our correct season
+    if s == season
+      # Go through the contestants (an array of objects)
+      info.select do |c|
+        # if their status is "Winner"
+        if c["status"] == "Winner"
+          # extract their first name from "name"
+          name_array = c["name"].split(' ')
+          first_name = name_array[0]
+        end
+
+      end
+
+    end
+
+  end
+# return first name
+first_name
 end
 
 def get_contestant_name(data, occupation)
